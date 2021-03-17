3df9579273431ddbebffae4471ffd357b07f209d
Alexis Ivy <627allie.ivy@gmail.com>
2021-03-17 15:54:45 -0500

    Modified razor pbs script with variables added for all elements

--- write_razor_pbs.py
+++ write_razor_pbs.py
@@ -5,15 +5,19 @@
 #This  script generates a PBS file for the AHPCC Razor Cluster
 
 # Define variables
+job_name = 'aivy'
 queue = 'med16core'
-wall = 3 # this is in hours
+job_out =  'aivy' # prefix for output file, hsould match the job name
+nnum =  1 #node number
+pnum = 1 #processor number
+wall = 3 #this is in hours
 
 #This section prints the header/required info for the PBS script
-print("#PBS -N test") #job name
+print("#PBS -N ", job_name) #job name
 print('#PBS -q', queue) #which queue to use
 print ('#PBS -j oe') #join the STDOUT and STDERR into a single file
-print ('#PBS -o aja.$PBS_JOBID') # set the name of the job out file
-print ('#PBS -l nodes=1:ppn=1') # how many resource to ask for (nodes = num nodes, ppn = num processors)
+print ('#PBS -o', job_out, '.$PBS_JOBID') # set the name of the job out file
+print ('#PBS -l nodes=',str(nnum),':ppn=',str(pnum)) # how many resource to ask for (nodes = num nodes, ppn = num processors)
 print ('#PBS -l walltime=' + str(wall) + ':00:00') # set the walltime w/in parameters of the queue (default = 1hr)
 print()
 
